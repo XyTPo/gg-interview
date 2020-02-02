61b445ec36c927ca023e9d4842f2fb2726e72325
INNOVECS\Roman.Mekhed <roman.mekhed@innovecs.com>
2020-02-02 23:01:48 +0200

    css fix

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const shareClick=document.getElementsByClassName('product__share-word');
+const closeClick=document.getElementsByClassName('close');
 
 Array.from(shareClick).forEach(function (element) {
     element.addEventListener('click', function () {
